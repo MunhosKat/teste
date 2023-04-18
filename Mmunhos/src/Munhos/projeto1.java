fb4a42223753f654117a94189875e7654e520a8d
MunhosKat <matheus.munhos2005@gmail.com>
2023-04-17 21:10:44 -0300

    aula mentoria

---
+++ Mmunhos/src/Munhos/projeto1.java
@@ -0,0 +1,23 @@
+package Munhos;
+import java.util.Scanner;
+
+public class projeto1 {
+
+	public static void main(String[] args) {
+	
+       Scanner entrada =new Scanner(System.in);
+       System.out.println("Digite seu número");
+       int n1 = entrada.nextInt();
+       int n2 = n1;
+       System.out.println(1 +n1);
+       System.out.println( -1 + n2);
+       int n3 = n1 * 2;
+       System.out.println(n3);
+       int n4 = n1 / 2;
+       System.out.println(n4);
+       
+       
+		
+	}
+
+}
---
+++ Mmunhos/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Munhos
+ *
+ */
+module Mmunhos {
+}
