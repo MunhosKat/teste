e4aafe2c246cb7b4690c5c4fce377b6c0e3f3112
MunhosKat <matheus.munhos2005@gmail.com>
2023-04-17 22:01:07 -0300

    final do projeto

--- Mmunhos/src/Munhos/projeto1.java
+++ Mmunhos/src/Munhos/projeto1.java
@@ -9,12 +9,14 @@ public class projeto1 {
        System.out.println("Digite seu número");
        int n1 = entrada.nextInt();
        int n2 = n1;
-       System.out.println(1 +n1);
-       System.out.println( -1 + n2);
+       
+       System.out.println("Seu número é:"+ (n1));
+       System.out.println("O Sucessor do seu número é: "+ (1 +n1));
+       System.out.println("O Antecessor do seu número é: "+(-1 + n2));
        int n3 = n1 * 2;
-       System.out.println(n3);
+       System.out.println("O Dobro do seu número é: " + n3);
        int n4 = n1 / 2;
-       System.out.println(n4);
+       System.out.println("A metade do seu número é: "+ (n4));
        
        
 		
